345810de79768a7cf38402d436d8c1948d6ef3a1
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-10-08 09:57:20 +0530

    Connection To Database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+        console.log('connection is sussessfull')
+    })
+    .catch((error)=>
+    {console.log(error.message)});
 
 const app = express();
 app.listen(3000,()=>{
     console.log("app is running in server 3000")
-})
+    console.log("app is running in server 3000");
+});
